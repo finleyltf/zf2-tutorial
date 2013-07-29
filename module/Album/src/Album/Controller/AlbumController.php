16e36063ab54966732130ff3bf7a0acd2be93a99
Tefu Lin <Tefu@Tefus-Macbook.local>
2013-07-30 00:55:42 +0300

    initialize the controller and model

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Tefu
+ * Date: 7/29/13
+ * Time: 11:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+
+}
+
